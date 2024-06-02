316dc4a7dcc319d6f3a965c6cc1ea217e367686a
MUKUL <mukul.2226csit1115@kiet.edu>
2024-06-02 16:13:07 +0530

    Middleware , token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,58 @@
+'use client'
 
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-function page() {
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("Nothing");
+
+  const getUserDetails = async () => {
+    const res = await axios.post("/api/users/me");
+    console.log(res.data.data._id);
+    setData(res.data.data._id);
+  };
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("logout success");
+      router.push("/login");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile Page </h1>
+      <hr />
+      <h2>
+        {data === "nothing" ? (
+          "Nothing "
+        ) : (
+          <Link href={`/profile/${data}`}>Test {data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={logout}
+      >
+        Logout
+      </button>
 
-export default page
+
+           <button
+        className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+            Get User Details
+      </button>
+    </div>
+  );
+}
